1a7e900a44ecafdd9230300326461c321fd28486
Ojeaga <ojeagabclifford@gmail.com>
2026-03-09 20:00:55 +0100

    checking some code

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const contactsController = require('../controllers/contacts');
 
-router.get('/contacts', contactsController.getAllContacts);
-router.get('/api/contacts/:id', contactsController.getContactById);
+// router.get('/api/contacts', contactsController.getAllContacts);
+router.get('/', contactsController.getAllContacts);
+router.get('/contacts/:id', contactsController.getContactById);
 
 module.exports = router;
